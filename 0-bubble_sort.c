7727182ba5a85495510b04dc3cea5d12535e04f8
slickback101 <kester.uzum@gmail.com>
2024-04-30 19:29:26 -0700

    eddit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,35 +10,26 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t a, b;
 	int temp;
-
-    if (array == NULL || size < 2)
-        return;
-
-    for (size_t b = 0; b < size - 1; b++)
-    {
-
-        /**Flag to optimize bubble sort by detecting if array is already sorted*/
-
-        int swapped = 0;
-
-        for (size_t a = 0; a < size - b - 1; a++)
-        {
-            if (array[a] > array[a + 1])
-            {
-                /** Swap elements*/
-                int temp = array[a];
-                array[a] = array[a + 1];
-                array[a + 1] = temp;
-
-                /** Set swapped flag */
-                swapped = 1;
-
-                /** Print the current array state */
-                print_array(array, size);
-            }
-        }
-
-        if (swapped == 0)
-            break;
-    }
+	
+	if (array == NULL || size < 2)
+		return;
+	
+	for (size_t b = 0; b < size - 1; b++)
+	{
+		int swapped = 0;
+		
+		for (size_t a = 0; a < size - b - 1; a++)
+		{
+			if (array[a] > array[a + 1])
+			{
+				int temp = array[a];
+				array[a] = array[a + 1];
+				array[a + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
